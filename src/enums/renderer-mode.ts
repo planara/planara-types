8ca37cf8b52db636d81079e5576b1a464a1bb325
a1unade <tags0956@gmail.com>
2025-10-06 15:02:00 +0300

    [add] rendering-mode

---
+++ src/enums/renderer-mode.ts
@@ -0,0 +1,14 @@
+/** Режимы рендеринга */
+export enum RendererMode {
+  /** Просмотр */
+  PLane = 'plane',
+
+  /** Xray */
+  Wireframe = 'wireframe',
+
+  /** Выбор элементов модели */
+  Select = 'select',
+
+  /** Выбор текстур для модели */
+  Texture = 'texture',
+}
