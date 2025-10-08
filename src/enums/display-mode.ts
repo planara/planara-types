fe386966dd1c6a47b1ff1b0cdb21b540b2e5464e
a1unade <tags0956@gmail.com>
2025-10-08 12:53:23 +0300

    [add] display-mode-enum

---
+++ src/enums/display-mode.ts
@@ -0,0 +1,11 @@
+/** Режимы отображения */
+export enum DisplayMode {
+    /** Просмотр */
+    PLane = 'plane',
+
+    /** Wireframe */
+    Wireframe = 'wireframe',
+
+    /** Текстурный режим */
+    Texture = 'texture'
+}
